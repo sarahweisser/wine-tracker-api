3ef3c00deb4c517407f18065f6222178869e197d
sweisser <weisser.sarah114@gmail.com>
2023-08-11 21:25:49 -0400

    initial commit

---
+++ src/main/java/com/sarahweisser/winetrackerapi/WineTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.sarahweisser.winetrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarahweisser/winetrackerapi/WineTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarahweisser.winetrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineTrackerApiApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
